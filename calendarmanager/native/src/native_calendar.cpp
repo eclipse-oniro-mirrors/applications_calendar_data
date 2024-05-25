7ddafedf7bdb3ce2d63a2a55a6c62f12d7d67bfb
xuzhiqiang <xuzhiqiang24@huawei.com>
2024-05-25 11:24:11 +0800

    2D接口能力增强
    Change-Id: Ia938935f162019a30fb3b4eea6ce9b1db68b85fd
    Signed-off-by: x00600181 <xuzhiqiang24@huawei.com>

--- calendarmanager/native/src/native_calendar.cpp
+++ calendarmanager/native/src/native_calendar.cpp
@@ -263,8 +263,7 @@ std::vector<Event> Calendar::GetEvents(std::shared_ptr<EventFilter> filter, cons
         setField(eventKey, queryField, resultSetField);
     } else {
         resultSetField = {"type", "title", "startTime", "endTime", "isAllDay", "description",
-        "timeZone", "location", "service", "attendee", "reminderTime",
-        "rrule", "identifier"};
+        "timeZone", "location", "service", "attendee", "reminderTime"};
     }
     DataShare::DatashareBusinessError error;
     auto result = DataShareHelperManager::GetInstance().Query(*(m_eventUri.get()),
