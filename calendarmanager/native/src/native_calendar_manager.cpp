ead6809d13ef6c8b52d050d341faf7e5ca0e516b
hid81661930 <2085927684@qq.com>
2026-05-11 11:54:47 +0800

    update: 更新文件 native_calendar_manager.cpp
    Signed-off-by: hid81661930 <2085927684@qq.com>

--- calendarmanager/native/src/native_calendar_manager.cpp
+++ calendarmanager/native/src/native_calendar_manager.cpp
@@ -78,6 +78,19 @@ Result<std::shared_ptr<Calendar>> CalendarManager::CreateCalendar(const Calendar
         return Result<std::shared_ptr<Calendar>>(error);
     }
     auto calendar = std::make_shared<Calendar>(account, result.GetValue());
+    // set default calendar config
+    auto calendarId = result.value();
+    DataShare::DataSharePredicates predicates;
+    const int64_t defaultColor = 0xFF0A59F7;
+    predicates.EqualTo("_id", calendarId);
+    DataShare::DataShareValuesBucket valuesBucket;
+    valuesBucket.Put("calendar_color", defaultColor);
+    valuesBucket.Put("canReminder", true);
+    auto ret = DataShareHelperManager::GetInstance()
+    .Update(*(m_calendarUri.get()), predicates, valuesBucket);
+    if (ret.is_err()) {
+        LOG_ERROR("native set default Config error, code: %{public}d", ret.error().code);
+    }
     return Result<std::shared_ptr<Calendar>>(calendar);
 }
 
